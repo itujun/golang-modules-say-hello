5474e37429d7530a65fad9c7ae782265219c7a6a
Juna <itujun7@gmail.com>
2025-07-17 19:45:55 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/itujun/golang-modules-say-hello
+
+go 1.24.4
---
+++ say_hello.go
@@ -0,0 +1,6 @@
+package golang_modules_say_hello
+
+// nama function harus diawali dengan huruf besar agar bisa diakses di luar package
+func SayHello() string {
+	return "Hello"
+}
