dae47aa0feda202428074c170cd9fa73faeac6ce
Juna <itujun7@gmail.com>
2025-07-17 20:50:34 +0700

    update return function

--- say_hello.go
+++ say_hello.go
@@ -2,5 +2,5 @@ package golang_modules_say_hello
 
 // nama function harus diawali dengan huruf besar agar bisa diakses di luar package
 func SayHello() string {
-	return "Hello"
+	return "Hello World!"
 }
