fb5ae527f7dee39b26f9115012bbf284d16cefdd
Juna <itujun7@gmail.com>
2025-07-17 20:59:10 +0700

    major upgrade

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/itujun/golang-modules-say-hello
+module github.com/itujun/golang-modules-say-hello/v2
 
 go 1.24.4
--- say_hello.go
+++ say_hello.go
@@ -1,6 +1,6 @@
 package golang_modules_say_hello
 
 // nama function harus diawali dengan huruf besar agar bisa diakses di luar package
-func SayHello() string {
-	return "Hello World!"
+func SayHelloi(user string) string {
+	return "Hello," + user
 }
